f8b3cd12dc89a3082a7ebce7a60a0e5d54ec77ab
Shivam Mathur <shivam_jpr@hotmail.com>
2020-05-26 15:04:53 +0530

    Refactor script and add support for focal

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,47 +1,49 @@
-LOG() {
+build_log() {
   time=$(date '+%Y-%m-%d %H:%M:%S')
   echo "$time"" > ""$1" >>build.log
 }
 
-release=$(lsb_release -r -s)
-install_dir=~/php/"$PHP_VERSION"
-action_dir=$(pwd)
-(
-  cd ~ || exit
-  git clone git://github.com/php-build/php-build
-  cd php-build || exit
-  sudo ./install.sh
-  sudo cp -rf "$action_dir"/.github/scripts/master /usr/local/share/php-build/definitions/master
-)
-sudo mkdir -p ~/php
-sudo chmod -R 777 ~/php
-php-build -i production master "$install_dir"
-sudo chmod 777 "$install_dir"/etc/php.ini
-(
-  echo "date.timezone=UTC"
-  echo "opcache.jit_buffer_size=256M"
-  echo "opcache.jit=1235"
-  echo "pcre.jit=1"
-) >>"$install_dir"/etc/php.ini
-sudo mkdir -p /usr/local/ssl
-sudo wget -O /usr/local/ssl/cert.pem https://curl.haxx.se/ca/cacert.pem
-curl -fsSL --retry 20 -O https://pear.php.net/go-pear.phar
-sudo chmod a+x ./.github/scripts/install-pear.sh
-./.github/scripts/install-pear.sh "$install_dir"
-rm go-pear.phar
-sudo "$install_dir"/bin/pear config-set php_ini "$install_dir"/etc/php.ini system
-sudo "$install_dir"/bin/pear config-set auto_discover 1
-sudo "$install_dir"/bin/pear channel-update pear.php.net
-sudo ln -sv "$install_dir"/sbin/php-fpm "$install_dir"/bin/php-fpm
-sudo ln -sf "$install_dir"/bin/* /usr/bin/
-sudo ln -sf "$install_dir"/etc/php.ini /etc/php.ini
-(
-  cd "$install_dir"/.. || exit
-  sudo XZ_OPT=-9 tar cfJ php_"$PHP_VERSION"+ubuntu"$release".tar.xz "$PHP_VERSION"
-  shopt -s nullglob
-  for f in *.xz; do
-    sha256sum "$f" >"${f}".sha256sum.txt
-  done
+setup_phpbuild() {
+  (
+    cd ~ || exit
+    git clone git://github.com/php-build/php-build
+    cd php-build || exit
+    sudo ./install.sh
+    sudo cp -rf "$action_dir"/.github/scripts/master /usr/local/share/php-build/definitions/master
+  )
+}
+
+setup_pear() {
+  sudo curl -fsSL --retry "$tries" -o /usr/local/ssl/cert.pem https://curl.haxx.se/ca/cacert.pem
+  sudo curl -fsSL --retry "$tries" -O https://pear.php.net/go-pear.phar
+  sudo chmod a+x .github/scripts/install-pear.expect
+  .github/scripts/install-pear.expect "$install_dir"
+  rm go-pear.phar
+  sudo "$install_dir"/bin/pear config-set php_ini "$install_dir"/etc/php.ini system
+  sudo "$install_dir"/bin/pear channel-update pear.php.net
+  sudo "$install_dir"/bin/pecl install -f pcov
+}
+
+build_php() {
+  if ! php-build -v -i production master "$install_dir"; then
+    echo 'Failed to build PHP'
+    exit 1
+  fi
+
+  sudo chmod 777 "$install_dir"/etc/php.ini
+  (
+    echo "date.timezone=UTC"
+    echo "opcache.jit_buffer_size=256M"
+    echo "opcache.jit=1235"
+    echo "pcre.jit=1"
+  ) >>"$install_dir"/etc/php.ini
+  setup_pear
+  sudo ln -sv "$install_dir"/sbin/php-fpm "$install_dir"/bin/php-fpm
+  sudo ln -sf "$install_dir"/bin/* /usr/bin/
+  sudo ln -sf "$install_dir"/etc/php.ini /etc/php.ini
+}
+
+bintray_create_package() {
   curl \
   --user "$BINTRAY_USER":"$BINTRAY_KEY" \
   --header "Content-Type: application/json" \
@@ -53,17 +55,38 @@ sudo ln -sf "$install_dir"/etc/php.ini /etc/php.ini
 \"public_stats\": true \
 }" \
   https://api.bintray.com/packages/"$BINTRAY_USER"/"$BINTRAY_REPO" || true
-  curl --user "$BINTRAY_USER":"$BINTRAY_KEY" -X DELETE https://api.bintray.com/content/"$BINTRAY_USER"/"$BINTRAY_REPO"/php_"$PHP_VERSION"+ubuntu"$release".tar.xz || true
-  curl --user "$BINTRAY_USER":"$BINTRAY_KEY" -T php_"$PHP_VERSION"+ubuntu"$release".tar.xz https://api.bintray.com/content/shivammathur/php/"$PHP_VERSION"-linux/"$PHP_VERSION"+ubuntu"$release"/php_"$PHP_VERSION"+ubuntu"$release".tar.xz || true
-  curl --user "$BINTRAY_USER":"$BINTRAY_KEY" -X POST https://api.bintray.com/content/"$BINTRAY_USER"/"$BINTRAY_REPO"/"$PHP_VERSION"-linux/"$PHP_VERSION"+ubuntu"$release"/publish || true
-)
+}
 
+build_and_ship() {
+  (
+    cd "$install_dir"/.. || exit
+    sudo XZ_OPT=-e9 tar cfJ php_"$PHP_VERSION"+ubuntu"$release".tar.xz "$PHP_VERSION"
+    curl --user "$BINTRAY_USER":"$BINTRAY_KEY" -X DELETE https://api.bintray.com/content/"$BINTRAY_USER"/"$BINTRAY_REPO"/php_"$PHP_VERSION"+ubuntu"$release".tar.xz || true
+    curl --user "$BINTRAY_USER":"$BINTRAY_KEY" -T php_"$PHP_VERSION"+ubuntu"$release".tar.xz https://api.bintray.com/content/shivammathur/php/"$PHP_VERSION"-linux/"$PHP_VERSION"+ubuntu"$release"/php_"$PHP_VERSION"+ubuntu"$release".tar.xz || true
+    curl --user "$BINTRAY_USER":"$BINTRAY_KEY" -X POST https://api.bintray.com/content/"$BINTRAY_USER"/"$BINTRAY_REPO"/"$PHP_VERSION"-linux/"$PHP_VERSION"+ubuntu"$release"/publish || true
+  )
+}
+
+push_log() {
 git config --local user.email "$GITHUB_EMAIL"
 git config --local user.name "$GITHUB_NAME"
 git stash
 git pull -f https://"$GITHUB_USER":"$GITHUB_TOKEN"@github.com/"$GITHUB_REPOSITORY".git HEAD:master
 git stash apply
-LOG "ubuntu$release build updated"
+build_log "ubuntu$release build updated"
 git add .
 git commit -m "ubuntu$release build updated"
 git push -f https://"$GITHUB_USER":"$GITHUB_TOKEN"@github.com/"$GITHUB_REPOSITORY".git HEAD:master --follow-tags
+}
+
+sudo mkdir -m777 -p /home/runner/php /usr/local/ssl
+release=$(lsb_release -r -s)
+install_dir=/home/runner/php/"$PHP_VERSION"
+action_dir=$(pwd)
+tries=10
+
+setup_phpbuild
+build_php
+bintray_create_package
+build_and_ship
+push_log
--- README.md
+++ README.md
@@ -4,21 +4,45 @@
 <a href="https://github.com/shivammathur/php-builder/blob/master/LICENSE" title="license"><img alt="LICENSE" src="https://img.shields.io/badge/license-MIT-428f7e.svg"></a>
 <a href="https://github.com/shivammathur/php-builder/tree/master/builds" title="builds"><img alt="PHP Versions Supported" src="https://img.shields.io/badge/php-%3E%3D%208.0.0-8892BF.svg"></a>
 
-> Build PHP nightly using GitHub Actions.
+> Build PHP nightly using GitHub Actions for Ubuntu.
 
 
 ## Builds
 
 - [Ubuntu 16.04](https://bintray.com/shivammathur/php/download_file?file_path=php_8.0%2Bubuntu16.04.tar.xz)
 - [Ubuntu 18.04](https://bintray.com/shivammathur/php/download_file?file_path=php_8.0%2Bubuntu18.04.tar.xz)
+- [Ubuntu 20.04](https://bintray.com/shivammathur/php/download_file?file_path=php_8.0%2Bubuntu20.04.tar.xz)
+
+
+## Install
+
+- Make sure sudo and curl are installed.
+```bash
+apt-get update
+apt-get install -y curl sudo
+```
+
+- Fetch the script and install.
+```bash
+curl -sSLO https://github.com/shivammathur/php-builder/releases/latest/download/install.sh
+chmod a+x ./install.sh
+./install.sh
+```
+
+- Test PHP
+```bash
+php -v
+```
 
 
 ## License
 
-The code in this project is licensed under the [MIT license](http://choosealicense.com/licenses/mit/).
-Please see the [license file](LICENSE) for more information.
+The code in this project is licensed under the [MIT license](LICENSE). This project has multiple [dependencies](#dependencies). Their licenses can be found in their respective repositories.
 
 
-## This project uses the following works
+## Dependencies
 
+- [PEAR](https://github.com/pear/pear-core)
+- [PCOV](https://github.com/krakjoe/pcov)
+- [PHP](https://github.com/php/php-src)
 - [php-build](https://github.com/php-build/php-build "php-build")
