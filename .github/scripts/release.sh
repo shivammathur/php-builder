0c8b127aa4ed24492c548f1b0c0609241a89c1cf
Shivam Mathur <shivam_jpr@hotmail.com>
2021-01-08 06:38:41 +0530

    Fix version check in release.sh

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -7,7 +7,7 @@ if [[ "$GITHUB_MESSAGE" != *no-ship* ]]; then
   done
   assets+=("./install.sh")
   for version in 8.0 8.1; do
-    tag=$(curl -sL https://api.github.com/repos/php/php-src/tags | grep -Po "(php-$version.[0-9]+)\"" | tail -n 1 | cut -d '"' -f 1)
+    tag=$(curl -sL https://www.php.net/releases/feed.php | grep -Po -m 1 "php-($version.[0-9]+)" | head -n 1)
     if [ "$tag" = "" ]; then tag='nightly'; fi
     echo "$tag" > "php$version.log"
     assets+=("./php$version.log")
