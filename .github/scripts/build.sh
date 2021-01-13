2d8747f68a249ba0ef234f6961527a4eef685b81
Shivam Mathur <shivam_jpr@hotmail.com>
2021-01-13 10:24:53 +0530

    Add support to switch SAPI --build-all

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -60,24 +60,29 @@ configure_apache_fpm_opts() {
 
 configure_apache_fpm() {
   sudo ln -sv "$install_dir"/sbin/php-fpm "$install_dir"/bin/php-fpm
+  sudo ln -sv "$install_dir"/bin/php-cgi "$install_dir"/usr/lib/cgi-bin/php"$PHP_VERSION"
+  sudo mv "$install_dir"/etc/init.d/php-fpm "$install_dir"/etc/init.d/php"$PHP_VERSION"-fpm
+  sudo mv "$install_dir"/usr/lib/apache2/modules/libphp.so "$install_dir"/usr/lib/apache2/modules/libphp"$PHP_VERSION".so
   sudo sed -Ei -e "s|^listen = .*|listen = /run/php/php$PHP_VERSION-fpm.sock|" -e 's|;listen.owner.*|listen.owner = www-data|' -e 's|;listen.group.*|listen.group = www-data|' -e 's|;listen.mode.*|listen.mode = 0660|' "$install_dir"/etc/php-fpm.d/www.conf
-  for file in fpm.service php-cgi.conf php-fpm.conf; do
+  sudo sed -i "s/PHP_MAJOR/${PHP_VERSION/%.*}/g" .github/scripts/php-fpm.conf
+  for file in default_nginx fpm.service php-cgi.conf php-fpm.conf php.load switch_sapi; do
     sudo sed -i "s/PHP_VERSION/$PHP_VERSION/g" .github/scripts/"$file"
   done
-  sudo sed -i "s/PHP_MAJOR/${PHP_VERSION/%.*}/g" .github/scripts/php-fpm.conf
   sudo sed -i "s/NO_DOT/${PHP_VERSION/./}/g" .github/scripts/fpm.service
   sudo cp -fp .github/scripts/fpm.service "$install_dir"/etc/systemd/system/php"$PHP_VERSION"-fpm.service
-  sudo cp -fp .github/scripts/php-fpm-socket-helper "$install_dir"/bin/ && sudo chmod a+x "$install_dir"/bin/php-fpm-socket-helper
+  sudo cp -fp .github/scripts/php-fpm-socket-helper .github/scripts/switch_sapi "$install_dir"/bin/ && sudo chmod -R a+x "$install_dir"/bin
   sudo sed -Ei -e "s|;pid.*|pid = /run/php/php$PHP_VERSION-fpm.pid|" -e "s|;error_log.*|error_log = /var/log/php$PHP_VERSION-fpm.log|" "$install_dir"/etc/php-fpm.conf
-  sudo mv /etc/apache2/mods-available/php.load "$install_dir"/etc/apache2/mods-available/php"$PHP_VERSION".load
   sudo cp -fp .github/scripts/php.conf "$install_dir"/etc/apache2/mods-available/php"$PHP_VERSION".conf
+  sudo cp -fp .github/scripts/php.load "$install_dir"/etc/apache2/mods-available/php"$PHP_VERSION".load
   sudo cp -fp .github/scripts/php-cgi.conf "$install_dir"/etc/apache2/conf-available/php"$PHP_VERSION"-cgi.conf
   sudo cp -fp .github/scripts/php-fpm.conf "$install_dir"/etc/apache2/conf-available/php"$PHP_VERSION"-fpm.conf
+  sudo cp -fp .github/scripts/default_apache "$install_dir"/etc/apache2/sites-available/000-default.conf
+  sudo cp -fp .github/scripts/default_nginx "$install_dir"/etc/nginx/sites-available/default
 }
 
 build_apache_fpm() {
   sudo cp /usr/local/share/php-build/default_configure_options.bak /usr/local/share/php-build/default_configure_options
-  sudo mkdir -p "$install_dir" "$install_dir"/"$(apxs -q SYSCONFDIR)"/mods-available "$install_dir"/"$(apxs -q SYSCONFDIR)"/conf-available /usr/local/ssl /lib/systemd/system
+  sudo mkdir -p "$install_dir" "$install_dir"/"$(apxs -q SYSCONFDIR)"/mods-available "$install_dir"/"$(apxs -q SYSCONFDIR)"/sites-available "$install_dir"/etc/nginx/sites-available "$install_dir"/"$(apxs -q SYSCONFDIR)"/conf-available "$install_dir"/usr/lib/cgi-bin /usr/local/ssl /lib/systemd/system /usr/lib/cgi-bin
   sudo chmod -R 777 "$install_dir" /usr/local/php /usr/local/ssl /usr/include/apache2 /usr/lib/apache2 /etc/apache2/ /var/lib/apache2 /var/log/apache2
   export PHP_BUILD_APXS="/usr/bin/apxs2"
   configure_apache_fpm_opts
@@ -113,12 +118,13 @@ configure_php() {
     echo "date.timezone=UTC"
     echo "memory_limit=-1"
   ) >>"$install_dir"/etc/php.ini
-  sudo cp -fp "$install_dir"/etc/init.d/php-fpm /etc/init.d/php"$PHP_VERSION"-fpm
-  sudo cp -fp "$install_dir"/etc/systemd/system/php-fpm.service /lib/systemd/system/php"$PHP_VERSION"-fpm.service
+  sudo cp -fp "$install_dir"/etc/init.d/php"$PHP_VERSION"-fpm /etc/init.d/
+  sudo cp -fp "$install_dir"/etc/systemd/system/php"$PHP_VERSION"-fpm.service /lib/systemd/system/
+  sudo cp -fp "$install_dir"/usr/lib/cgi-bin/php"$PHP_VERSION" /usr/lib/cgi-bin/
+  sudo cp -fp "$install_dir"/usr/lib/apache2/modules/libphp"$PHP_VERSION".so /usr/lib/apache2/modules/
   sudo cp -fp "$install_dir"/etc/apache2/mods-available/php"$PHP_VERSION".load /etc/apache2/mods-available/php"$PHP_VERSION".load
-  sudo cp -fp .github/scripts/php.conf /etc/apache2/mods-available/php"$PHP_VERSION".conf
-  sudo cp -fp .github/scripts/php-cgi.conf /etc/apache2/conf-available/php"$PHP_VERSION"-cgi.conf
-  sudo cp -fp .github/scripts/php-fpm.conf /etc/apache2/conf-available/php"$PHP_VERSION"-fpm.conf
+  sudo cp -fp "$install_dir"/etc/apache2/mods-available/php"$PHP_VERSION".conf /etc/apache2/mods-available/
+  sudo cp -fp "$install_dir"/etc/apache2/conf-available/php"$PHP_VERSION"-*.conf /etc/apache2/conf-available/
   sudo a2dismod php
   if ! sudo service php"$PHP_VERSION"-fpm start; then
     journalctl -xe
@@ -143,12 +149,9 @@ bintray_create_package() {
 
 build_and_ship() {
   (
-    curl -sSLO http://archive.ubuntu.com/ubuntu/pool/main/g/gcc-10/gcc-10-base_10-20200411-0ubuntu1_amd64.deb
-    curl -sSLO http://archive.ubuntu.com/ubuntu/pool/main/g/gcc-10/libgcc-s1_10-20200411-0ubuntu1_amd64.deb
-    curl -sSLO http://archive.ubuntu.com/ubuntu/pool/universe/libz/libzstd/zstd_1.4.4+dfsg-3_amd64.deb
-    sudo DEBIAN_FRONTEND=noninteractive dpkg -i --force-conflicts ./*.deb
-    rm -rf ./*.deb
-    zstd -V
+    export PATH="$HOME/.linuxbrew/bin:$PATH"
+    echo "export PATH=$HOME/.linuxbrew/bin:\$PATH" >> "$GITHUB_ENV"
+    brew install zstd >/dev/null 2>&1 && zstd -V
     cd "$install_dir"/.. || exit
     sudo XZ_OPT=-e9 tar cfJ php_"$PHP_VERSION"+ubuntu"$release".tar.xz "$PHP_VERSION"
     sudo tar cf - "$PHP_VERSION" | zstd -22 -T0 --ultra > php_"$PHP_VERSION"+ubuntu"$release".tar.zst
