baeb58cbf3227f6fbf577e8bdbfc16d38a4aa95e
Shivam Mathur <shivam_jpr@hotmail.com>
2021-03-22 09:28:11 +0530

    Fix pecl extensions --build-all

--- scripts/build.sh
+++ scripts/build.sh
@@ -30,9 +30,24 @@ setup_pear() {
   sudo "$install_dir"/bin/pear channel-update pear.php.net
 }
 
+enable_extension() {
+  extension=$1
+  if [ "$extension" = "xdebug" ]; then
+    echo "zend_extension=$extension.so" | sudo tee -a "$install_dir"/etc/conf.d/20-"$extension".ini
+  else
+    echo "extension=$extension.so" | sudo tee -a "$install_dir"/etc/conf.d/20-"$extension".ini
+  fi
+}
+
 setup_extensions() {
-  for extension in amqp-1.11.0beta apcu igbinary memcache msgpack pcov sqlsrv pdo_sqlsrv xdebug; do
-    yes '' | sudo "$install_dir"/bin/pecl install -f "$extension"
+  ext_dir="$("$install_dir"/bin/php -i | grep "extension_dir => /" | sed -e "s|.*=> s*||")"
+  for package in amqp-1.11.0beta apcu igbinary memcache msgpack pcov sqlsrv pdo_sqlsrv xdebug; do
+    extension=${package%-*}
+    yes '' 2>/dev/null | sudo "$install_dir"/bin/pecl install -f "$package"
+    sudo sed -i "/$extension/d" "$install_dir"/etc/php.ini
+    if [ -e "$ext_dir/$extension.so" ]; then
+      enable_extension "$extension"
+    fi
   done
   sudo bash scripts/install-ext.sh imagick extension Imagick/imagick master "$install_dir"
   sudo bash scripts/install-ext.sh memcached extension php-memcached-dev/php-memcached master "$install_dir"
