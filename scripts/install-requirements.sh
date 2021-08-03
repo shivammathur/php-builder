e1a69394c3790f7b5f600730f664bdb4c7b8f17b
Shivam Mathur <shivam_jpr@hotmail.com>
2021-08-03 06:32:13 +0530

    Add Ubuntu 21.04 build --build-all

--- scripts/install-requirements.sh
+++ scripts/install-requirements.sh
@@ -18,21 +18,15 @@ elif [ "$ID" = "ubuntu" ]; then
   fi
 fi
 
-if [ "$VERSION_ID" = "16.04" ] || [ "$VERSION_ID" = "9" ]; then
-  libs=("libgccjit-6-dev" "libenchant-dev")
-elif [ "$VERSION_ID" = "18.04" ] || [ "$VERSION_ID" = "10" ]; then
-  libs=("libgccjit-8-dev" "libenchant-dev")
-elif [ "$VERSION_ID" = "20.04" ] || [ "$VERSION_ID" = "11" ]; then
-  libs=("libgccjit-10-dev" "libenchant-2-dev")
-fi
-
 apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y autoconf gcc-9 g++-9 expect libaio1 locales re2c "$mysql" postgresql pkg-config apache2 apache2-dev libapache2-mod-fcgid libaspell-dev libbz2-dev libbison-dev libedit-dev libcurl4-gnutls-dev libffi-dev libfreetype6-dev "${libs[@]}" libargon2-dev libmagickwand-dev libgmp-dev libicu-dev libjpeg-dev libwebp-dev libc-client2007e-dev libkrb5-dev libldb-dev libldap-dev liblz4-dev libonig-dev libmcrypt-dev libmemcached-dev libpng-dev libpq5 libpq-dev librabbitmq-dev libreadline-dev libpspell-dev libsasl2-dev libsnmp-dev libssl-dev libsqlite3-dev libsodium-dev libtidy-dev libwebp-dev libxml2-dev libxpm-dev libxslt1-dev libzip-dev libzstd-dev zlib1g liblzma-dev liblz4-dev unixodbc-dev
+lib_enchant=$(apt-cache show libenchant-?[0-9]+?-dev | grep 'Package' | head -n 1 | cut -d ' ' -f 2)
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y autoconf gcc-9 g++-9 expect libaio1 locales re2c "$mysql" postgresql pkg-config apache2 apache2-dev libapache2-mod-fcgid libaspell-dev libbz2-dev libbison-dev libedit-dev "$lib_enchant" libcurl4-gnutls-dev libffi-dev libfreetype6-dev libargon2-dev libmagickwand-dev libgmp-dev libicu-dev libjpeg-dev libwebp-dev libc-client2007e-dev libgccjit-9-dev libkrb5-dev libldb-dev libldap-dev liblz4-dev libonig-dev libmcrypt-dev libmemcached-dev libpng-dev libpq5 libpq-dev librabbitmq-dev libreadline-dev libpspell-dev libsasl2-dev libsnmp-dev libssl-dev libsqlite3-dev libsodium-dev libtidy-dev libwebp-dev libxml2-dev libxpm-dev libxslt1-dev libzip-dev libzstd-dev zlib1g liblzma-dev liblz4-dev unixodbc-dev
 ln -sf /usr/lib/libc-client.so.2007e.0 /usr/lib/x86_64-linux-gnu/libc-client.a
-mkdir -p /usr/c-client/
+mkdir -p /usr/c-client/ /usr
 ln -sf /usr/lib/libc-client.so.2007e.0 /usr/c-client/libc-client.a
-if [ "$VERSION_ID" = "20.04" ] || [ "$VERSION_ID" = "11" ]; then
-  mkdir -p /usr/lib64
+ln -s /usr/lib/x86_64-linux-gnu/libldap.so /usr/lib/libldap.so
+ln -s /usr/lib/x86_64-linux-gnu/liblber.so /usr/lib/liblber.so
+if [ -d /usr/lib64 ]; then
   ln -s /usr/lib/x86_64-linux-gnu/libldap.so /usr/lib64/libldap.so
   ln -s /usr/lib/x86_64-linux-gnu/liblber.so /usr/lib64/liblber.so
 fi
