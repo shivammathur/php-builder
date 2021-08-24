06e94236ee172c0b9b6210f15f7783764f41c83f
Shivam Mathur <shivam_jpr@hotmail.com>
2021-08-24 23:07:14 +0530

    Fix imagick version

--- scripts/install-extension.sh
+++ scripts/install-extension.sh
@@ -3,6 +3,7 @@
 # Function to patch imagick source.
 patch_imagick() {
   sed -i 's/spl_ce_Countable/zend_ce_countable/' imagick.c util/checkSymbols.php
+  sed -i "s/@PACKAGE_VERSION@/$(grep -Po 'release>\K(\d+\.\d+\.\d+)' package.xml)/" php_imagick.h
 }
 
 # Function to patch sqlsrv source.
