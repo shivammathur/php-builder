85def648ee8aa16d748fc46034b464085fa96762
Shivam Mathur <shivam_jpr@hotmail.com>
2022-11-05 06:24:23 +0530

    Fix extension version in test_pecl --build-all

--- scripts/test_pecl.sh
+++ scripts/test_pecl.sh
@@ -1,5 +1,5 @@
 if [ "$PHP_VERSION" = "5.6" ]; then
-  sudo pecl install -f psr-0.6.1
+  sudo pecl install -f psr-0.6.0
 elif [[ "$PHP_VERSION" =~ 7.[0-2]$ ]]; then
   sudo pecl install -f psr-1.1.0
 else
