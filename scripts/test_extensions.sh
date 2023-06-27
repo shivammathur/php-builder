b3c2deb0dfd2a2f04de45d940dbf74943233c72c
Shivam Mathur <shivam_jpr@hotmail.com>
2023-06-27 12:21:19 +0000

    Do not check for ds on PHP 5.6

--- scripts/test_extensions.sh
+++ scripts/test_extensions.sh
@@ -1,6 +1,9 @@
 set -e
 php -m
-extensions=( "amqp" "apcu" "ds" "igbinary" "imagick" "memcache" "memcached" "mongodb" "msgpack" "redis" "xdebug" "sqlsrv" "pdo_sqlsrv" "yaml" )
+extensions=( "amqp" "apcu" "igbinary" "imagick" "memcache" "memcached" "mongodb" "msgpack" "redis" "xdebug" "sqlsrv" "pdo_sqlsrv" "yaml" )
+if [[ "$PHP_VERSION" != "5.6" ]]; then
+  extensions+=( "ds" )
+fi
 if [[ "$PHP_VERSION" != "5.6" && "$PHP_VERSION" != "7.0" ]]; then
    extensions+=( "pcov" )
    ln -sf /etc/php/"$PHP_VERSION"/mods-available/pcov.ini /etc/php/"$PHP_VERSION"/cli/conf.d/20-pcov.ini
